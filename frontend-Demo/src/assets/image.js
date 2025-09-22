39c8c90e98cacf33971ab40e371efd18394c157f
Vishal Kumar <vishalyadav6jd@gmail.com>
2025-09-22 23:15:24 +0530

    update version

--- frontend-Demo/src/assets/image.js
+++ frontend-Demo/src/assets/image.js
@@ -1,14 +1,14 @@
-import EcommIcon from "../images/icon.png";
-import Paypal from "../images/Paypal.png";
-import WesternUnion from "../images/Western-union.png";
+import EcommIcon from "./images/icon.png";
+import Paypal from "./images/Paypal.png";
+import WesternUnion from "./images/Western-union.png";
 import MasterCard from "./images/Group 19.png";
-import Shoe_1 from "../images/banner-shoe.png";
-import menuSwitch from "../images/menuSwitch.png";
-import expandSwitch from "../images/expandSwitch.png";
-import Shoe_2 from "../images/Shoe_2.png";
-import Shoe_3 from "../images/Shoe_3.png";
-import Shoe_4 from "../images/Shoe_4.png";
-import Shoe_5 from "../images/Shoe_5.png";
+import Shoe_1 from "./images/banner-shoe.png";
+import menuSwitch from "./images/menuSwitch.png";
+import expandSwitch from "./images/expandSwitch.png";
+import Shoe_2 from "./images/Shoe_2.png";
+import Shoe_3 from "./images/Shoe_3.png";
+import Shoe_4 from "./images/Shoe_4.png";
+import Shoe_5 from "./images/Shoe_5.png";
 
 export {
   EcommIcon,
